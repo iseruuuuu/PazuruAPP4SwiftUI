2a576765550b0d0095afb4eb024c0ab41267aa46
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-01-26 11:24:57 +0900

    Initial Commit

---
+++ PazuruAPP4SwiftUI/PazuruAPP4SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PazuruAPP4SwiftUIApp.swift
+//  PazuruAPP4SwiftUI
+//
+//  Created by 井関竜太郎 on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PazuruAPP4SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
